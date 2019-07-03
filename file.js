7dcd6fdbdf7063d091135de238a7741e83653b7f
Khemiri abdelmoneem <abd_mon_kh@hotmail.com>
2019-07-03 23:45:03 +0200

    express

--- file.js
+++ file.js
@@ -1,1 +1,10 @@
-const express=1
+artistRouter.delete('/:artistId',(req,res,next)=>{
+    const sql=`UPDATE Artist SET is_currently_employed=0 WHERE id=${req.params.artistId}`;
+    db.run(sql,(err)=>{
+        if(err){
+            next(err)
+        }else{
+            res.status(200)
+        }
+    })
+})
