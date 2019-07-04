dce479672ef0078d45b4ea11398d20fd934bda6c
Khemiri abdelmoneem <abd_mon_kh@hotmail.com>
2019-07-04 02:14:22 +0200

    x-press

--- file.js
+++ file.js
@@ -1,1 +1,45 @@
-const express=1
+const express=require('express')
+//open migration.js
+db.serialize(()=>{
+    db.run('DROP TABLE IF NOT EXISTS Series');
+    db.run('CREATE TABLE Series(id INTEGER PRIMARY KEY,name TEXT ,description TEXT')
+
+})
+//create series.js in /api directory
+//open series.js
+const seriesRouter=express.Router()
+const sqlite3=require('sqlite3')
+const db=new sqlite3.Database(process.env.DATABASE||../migration.js)
+
+
+
+seriesRouter.param('seriesId',(req,res,next,seriesId)=>{
+    db.get('SELECT * FROM Series WHERE id=$seriesId',{$seriesId:seriesId},(err,row)=>{
+        if(err){
+          next(err)}  
+        else if(row){
+            req.series=row
+            next()
+        }
+        else{
+            res.status(404)send()
+        }
+    })
+})
+seriesRouter.get('/',(req,res,next)=>{
+    db.all('SELECT * FROM Series',(err,rows)={
+        if(err){
+            next(err)
+        }else{
+            res.status(200).json({series:rows})
+        }
+    })
+})
+
+
+
+
+
+
+module.exports=seriesRouter
+//open api.js and write seriesRouter=require(./series.js)
